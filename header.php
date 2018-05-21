5f402f8d2a5856021da7e49fcea98e5fdc1d7282
Tieu Quoc Nghi <quocnghi13@gmail.com>
2018-05-21 15:29:45 +0700

    fixed signin

---
+++ header.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+<title></title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+<style>
+body {font-family: "Lato", sans-serif}
+.mySlides {display: none}
+#gg {
+    color: black !important;
+}
+#aa {
+    color: black !important;
+}
+</style>
+
+ 
+<body>
+ 
+
+<!-- Navbar -->
+
+
+<div class="w3-top">
+  <div class="w3-bar w3-black w3-card">
+    <a class="w3-bar-item w3-button w3-padding-large w3-hide-medium w3-hide-large w3-right" href="javascript:void(0)" onclick="myFunction()" title="Toggle Navigation Menu"><i class="fa fa-bars"></i></a>
+    <a href="signin.php" class="w3-bar-item w3-button w3-padding-large">Đăng Nhập</a>
+    <a href="signup.php" class="w3-bar-item w3-button w3-padding-large w3-hide-small">Đăng Ký</a>
+        
+   
+
+    
+
+   
+
+
+    <!-- <a href="javascript:void(0)" class="w3-padding-large w3-hover-gray w3-hide-small w3-right"><i class="fa fa-search"></i></a> -->
+  </div>
+</div>
+
+
+
+
+
+
+
+
+
+</body>
+</html>
--- signup.php
+++ signup.php
@@ -1,4 +1,6 @@
-
+<?php
+  include 'header.php'
+?>
 <?php 
      session_start();
  ?>
